b234026284248ee49210acfffc580a4b0c3995d0
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-08-05 13:40:44 +0300

    Fix rights

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,6 +3,7 @@
 set -xe
 #Запишем креды в файл для юнит сервиса
 sudo sh -c 'printf "PSQL_USER=$PSQL_USER\nPSQL_PASSWORD=$PSQL_PASSWORD\nPSQL_HOST=$PSQL_HOST\nPSQL_PORT=$PSQL_PORT\nPSQL_DBNAME=$PSQL_DBNAME\nPSQL_ADMIN=$PSQL_ADMIN\nMONGO_HOST=$MONGO_HOST\nMONGO_USER=$MONGO_USER\nMONGO_PASSWORD=$MONGO_PASSWORD\nMONGO_DATABASE=$MONGO_DATABASE" > /home/jarservice/creds'
+sudo chown jarservice:jarservice /home/jarservice/creds
 #Перезаливаем дескриптор сервиса на ВМ для деплоя
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
 sudo rm -f /home/jarservice/sausage-store.jar||true
