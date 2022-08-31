9586988b81d7baa999a547590d4df0abc49533a7
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-08-31 23:31:39 +0300

    Fix ci

---
+++ vault_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+cat <<EOF | docker exec -i vault ash
+  sleep 10;
+  vault login ${VAULT_TOKEN}
+
+  vault secrets enable -path=secret kv
+
+  vault kv put secret/sausage-store spring.datasource.password="${SPRING_DATASOURCE_PASSWORD}" \
+   spring.data.mongodb.uri="${MONGODB_URL}" \
+   spring.datasource.url="${PSQL_DATASRC}"
+
+  vault kv put secret/sausage-store-test spring.datasource.username="${TEST_DATASRC_PSQL_USER}"
+                                         spring.datasource.password="${TEST_DATASRC_PSQL_PASS}"
+EOF
