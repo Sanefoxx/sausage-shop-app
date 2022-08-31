3d5d8da3f8ae5ab1db0f0c343ed63c62fb64a536
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-08-31 18:11:48 +0300

    Change deploy to docker-compose

--- backend-report/backend_report_deploy.sh
+++ backend-report/backend_report_deploy.sh
@@ -2,8 +2,8 @@
 set +e
 sudo cp -rf docker-compose.yml /home/jarservice/docker_compose/docker-compose.yml
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
-docker stop sausage-store-frontend || true
-docker rm sausage-store-frontend || true
+docker stop sausage-store-backend || true
+docker rm sausage-store-backend || true
 set -e
 cd /home/jarservice/docker_compose/
 docker-compose up -d
