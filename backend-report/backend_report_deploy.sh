2d55670c6643bb5ac428583a731266582ad8eaa1
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-08-31 21:26:07 +0300

    Fix ci

--- backend-report/backend_report_deploy.sh
+++ backend-report/backend_report_deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set +e
+cat > .env <<EOF
+VAULT_HOST=${VAULT_HOST}
+VAULT_TOKEN=${VAULT_TOKEN}
+EOF
 sudo cp -rf docker-compose.yml /home/jarservice/docker_compose/docker-compose.yml
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 docker stop sausage-store-backend-report || true
