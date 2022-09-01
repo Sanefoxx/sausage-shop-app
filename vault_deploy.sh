555dabd54e4f1bcbc30cf75df4ff7e4166588521
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-09-01 11:07:02 +0300

    Fix ci

--- vault_deploy.sh
+++ vault_deploy.sh
@@ -5,6 +5,7 @@ docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 echo "222222"
 docker-compose stop sausage-store-vault
 docker-compose up -d sausage-store-vault
+echo "33333"
 cat <<EOF | docker exec -i sausage-store-vault ash
   sleep 10;
   vault login ${VAULT_TOKEN}
