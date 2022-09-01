2677120c1ffaa5dd60e2ea648d8acd52894b6752
Sanefoxx <alexndr.lisitsin@gmail.com>
2022-09-01 12:33:46 +0300

    Fix ci

--- vault_deploy.sh
+++ vault_deploy.sh
@@ -3,7 +3,8 @@ set -e
 echo "111111"
 docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
 echo "222222"
-docker stop sausage-store-vault
+docker stop sausage-store-vault|| true
+docker rm sausage-store-backend || true
 echo "33333"
 docker-compose up -d --no-deps --build sausage-store-vault
 echo "4444444"
